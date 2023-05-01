1dfd388092b7512b4b7a8264a4eaa063b80bb4f2
chanwoo <chanwoo0628@gmail.com>
2023-05-01 23:12:29 +0900

    [UPDATE] pymysql import 코드 추가

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,8 @@ import os
 import configuration
 from pathlib import Path
 
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
