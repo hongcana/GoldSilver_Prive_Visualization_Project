c78f38a3d7b2d1e714b2d013722cfba4e041ee5b
chanwoo <chanwoo0628@gmail.com>
2023-05-02 13:15:31 +0900

    [UPDATE] 민감정보 마스킹

--- configuration.py
+++ configuration.py
@@ -1,5 +1,7 @@
 # 데이터베이스 접속 비밀번호
 DB_PASSWORD = ''
+HOST = ''
+USER = ''
 
 SECRET_KEY = 'django-insecure--@bw(8tp-_&waq2u(xk_=0g7zl_((e=r*_xb!^n&gpd^-yu!i0'
 
@@ -7,9 +9,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # engine: mysql
         'NAME' : 'sys', 
-        'USER' : 'admin', # DB User
+        'USER' : USER, # DB User
         'PASSWORD' : DB_PASSWORD, 
-        'HOST': 'goldsilverprice-db.c4ca5hhwyuky.ap-northeast-2.rds.amazonaws.com',
+        'HOST': HOST,
         'PORT': '3306', # 데이터베이스 포트
         'OPTIONS':{
             'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
