8a809192a8dced5e17fd1d6afa84c28105d63fdf
chanwoo <chanwoo0628@gmail.com>
2023-05-02 16:33:12 +0900

    [ADD] PriceDashBoard 어플리케이션 구성파일 추가

---
+++ PriceDashBoard/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class MaterialsModel(models.Model):
+    material_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.material_name} info'
+
+
+class MaterialsPriceModel(models.Model):
+    material_name = models.ForeignKey(MaterialsModel, on_delete=models.CASCADE)
+    date = models.DateField()
+    price = models.FloatField()
+    
+    def __str__(self):
+        return f'{self.date} 자 {self.material_name} 가격 : {self.price}'
